64e1cf007765a95fb9f679df895d4f3b99df32f4
Beman <bdawes@acm.org>
2016-02-02 06:54:18 -0500

    Add comments to meta-data/h-style-index scripts.

--- meta-data/h-style-index.bat
+++ meta-data/h-style-index.bat
@@ -1,11 +1,11 @@
 @echo off
+rem Prerequisite: POSIX grep and sed, such as from Cygwin
 if %1$==$ goto error
 if %2$==$ goto error
 if %3$==$ goto error
 grep "<h[1-9]" %2 | sed -r --file=h-style-index.sed | grep "section-prefix" | sed -r "s/section-prefix/%1/g" >%3
 echo Complete. See %3
-echo Note that headings split between multiple lines are not handled correctly
-echo and first must be edited down to a single line.
+echo TODO: Handle headings split across two lines. Filesystem TS needs that.
 goto ok
 
 :error
--- meta-data/h-style-index.sed
+++ meta-data/h-style-index.sed
@@ -1,3 +1,6 @@
+# Extract n.n... [xx.xxxx...] from HTML headers (e.g. "<h3>1.2.3 Title [my.title]</h3>").
+# See h-style-index.bat for invokation.
+# TODO: Splice lines together first if headers extend across several lines
 s/<h[1-9]>[^0-9]*([0-9.]*)[^[]*\[([^]]*).*/section-prefix \1 [\2]/g
 s/([^<]*)<[^>]*>(.*)/\1\2/g
 s/([^<]*)<[^>]*>(.*)/\1\2/g
