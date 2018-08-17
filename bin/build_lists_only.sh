19d3664885127bc709c281eab701c48eedf558ad
Casey Carter <Casey@Carter.net>
2018-08-17 15:12:37 -0700

    Don't let the shell expand the *s in build_lists_only.sh

--- bin/build_lists_only.sh
+++ bin/build_lists_only.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 bin/lists && test -f mailing/lwg-active.html && exit
-echo ***********************************
-echo ********** build failure **********
-echo ***********************************
+echo "***********************************"
+echo "********** build failure **********"
+echo "***********************************"
 exit 1
