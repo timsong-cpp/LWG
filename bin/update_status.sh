1f32feaaf421323b1356e05e18343a9d248f22ac
Jonathan Wakely <cxx@kayari.org>
2023-06-09 15:41:20 +0100

    Add comment to update_status.sh script

--- bin/update_status.sh
+++ bin/update_status.sh
@@ -2,4 +2,10 @@
 while read issue_number           
 do           
     bin/set_status $issue_number "$@"
+
+# To add a comment use the following (changing the date and text!)
+#    sed -i '/<\/discussion/i\
+#<note>2022-07-15; LWG telecon: move to Ready</note>\
+#' xml/issue$issue_number.xml
+
 done
