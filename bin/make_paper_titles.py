045ef7a73ba848445022731cba44e4457b4b409f
Jonathan Wakely <cxx@kayari.org>
2024-01-29 13:25:17 +0000

    Generate title popups for Pnnnn papers without an Rn revision

--- bin/make_paper_titles.py
+++ bin/make_paper_titles.py
@@ -4,7 +4,19 @@ import json
 import sys
 import re
 
+latest_rev = {}
+
 with open(sys.argv[1]) as f:
-    for num,data in json.load(f).items():
-        if re.match(r'^(P\d\d\d\dR\d+|N\d\d\d\d)$', num):
+    docs = json.load(f)
+    for num,data in docs.items():
+        m = re.fullmatch(r'(P\d\d\d\d)R(\d+)|N\d\d\d\d', num)
+        if m:
             print("{} {}".format(num, data['title']))
+            if num.startswith('P'):
+                pnum = m[1]
+                rev = int(m[2])
+                if rev > latest_rev.get(pnum, -1):
+                    latest_rev[pnum] = rev
+    for pnum, rev in latest_rev.items():
+        pnumrev = "{}R{}".format(pnum, rev)
+        print("{} {}".format(pnum, docs[pnumrev]['title']))
